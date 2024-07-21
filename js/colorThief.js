79390e9c5d1c33fef5c48e8acfde1ed6a2a9366c
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2024-07-21 00:27:17 -0500

    better pic

--- js/colorThief.js
+++ js/colorThief.js
@@ -8,6 +8,8 @@ var css = 'linear-gradient(180deg, rgba('+color[0]+','+color[1]+','+color[2]+',1
 $(document).ready(function() { 
     $('#main').css('background', css);
     $('.particles-js').css('background', css);
+/*     $('.profilePic').css('background', 'rgb('+color[0]+','+color[1]+','+color[2]+',1)');
+ */
 
 })
 
