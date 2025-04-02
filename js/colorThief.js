e541f55689bbab78e230202793902422c5e7c05a
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2025-04-02 11:12:11 -0500

    Better design and experience in mobile

--- js/colorThief.js
+++ js/colorThief.js
@@ -1,9 +1,8 @@
-import ColorThief from '../node_modules/colorthief/dist/color-thief.mjs'
+import ColorThief from '../node_modules/colorthief/dist/color-thief.mjs';
 
 const colorThief = new ColorThief();
 const img = document.querySelector('.profilePic');
 var color = colorThief.getColor(img);
-console.log(color[0]);
 var css = 'linear-gradient(180deg, rgba('+color[0]+','+color[1]+','+color[2]+',1) 26%, rgba(69,69,70,1) 100%)';
 $(document).ready(function() { 
     $('#main').css('background', css);
