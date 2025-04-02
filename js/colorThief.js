bd3c447e07153ec630806566f1f0172bab9d8056
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2025-04-02 13:55:10 -0500

    ola

--- js/colorThief.js
+++ js/colorThief.js
@@ -8,7 +8,7 @@ var css = 'linear-gradient(180deg, rgba(42, 0, 78, 0.4) 26%, rgba(69,69,70,1) 10
 /* var css = 'linear-gradient(180deg, rgba('+color[0]+','+color[1]+','+color[2]+',1) 26%, rgba(69,69,70,1) 100%)';
  */$(document).ready(function() { 
     $('#main').css('background', css);
-    $('.particles-js').css('background', css);
+    $('.particles-js').css  ('background', css);
 /*     $('.profilePic').css('background', 'rgb('+color[0]+','+color[1]+','+color[2]+',1)');
  */
 
